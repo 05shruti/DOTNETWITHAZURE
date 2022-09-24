e49248f20f0bb9a7a67522a085d5dfb5ca2aeabd
unknown <shrutipandey99997@gmail.com>
2022-09-24 20:19:18 +0530

    first commit

---
+++ DAY 6/Day 6Class6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Class1
+{
+    static int maxCHARS = 256;
+    static void calculate(string s, int[] cal)
+    {
+        for (int i = 0; i < s.Length; i++)
+            cal[s[i]]++;
+    }
+    public static void Main()
+    {
+        string s = "aabbcccdddeee";
+        int[] cal = new int[maxCHARS];
+        calculate(s, cal);
+        for (int i = 0; i < maxCHARS; i++)
+            if (cal[i] > 1)
+            {
+                Console.WriteLine("Character" + (char)i);
+                Console.WriteLine("Occurence =" + cal[i] + "times");
+            }
+    }
+}
