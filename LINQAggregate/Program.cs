4ff068dcdc35bd3c6e435046e748b03341bbdb61
unknown <shrutipandey99997@gmail.com>
2022-09-27 15:15:18 +0530

    first commit

---
+++ LINQAggregate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQAggregate
+{
+    class Program
+    {
+    
+
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 8, 3, 2 };
+            double result = list.Average();
+        }
+    }
+}
