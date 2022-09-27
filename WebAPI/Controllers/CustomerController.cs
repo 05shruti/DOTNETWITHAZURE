bdaa826c4fbfebd05146e5bb16a417d9168bc635
unknown <shrutipandey99997@gmail.com>
2022-09-27 15:17:03 +0530

    first commit

---
+++ WebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        CustomerDBContext db = new CustomerDBContext();
+        [HttpGet]
+        public IEnumerable<Customer> get()
+        {
+            return db.Customers;
+        }
+        [HttpPost]
+        public IActionResult post(Customer customer)
+        {
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            return Ok(new { status = "your record is added suceessfully" });
+        }
+    }
+}
