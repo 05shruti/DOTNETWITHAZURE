faa4445578af4d6d170ee80533e022ecfdce3fb0
unknown <shrutipandey99997@gmail.com>
2022-09-27 15:10:43 +0530

    first commit

---
+++ LINQ1.0/Models/TblEmployees.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace LINQ1._0.Models
+{
+    public partial class TblEmployees
+    {
+        public int Id { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeSalary { get; set; }
+    }
+}
---
+++ LINQ1.0/Models/TestDBContext.cs
@@ -0,0 +1,9 @@
+﻿namespace ConnectWithDB
+{
+    internal class TestDBContext
+    {
+        public object TblSubDepartments { get; internal set; }
+        public object TblDepartments { get; internal set; }
+        public object TblEmployees { get; internal set; }
+    }
+}
