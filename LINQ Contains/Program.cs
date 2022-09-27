7bced166df318bcc2bf9765c202b17b0afd775ac
unknown <shrutipandey99997@gmail.com>
2022-09-27 15:04:28 +0530

    first commit

---
+++ LINQ Contains/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace LINQDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namesList = new List<string>() { "Shruti", "Mandeep", "Chuchu", "Pandey", "Singh" };
+            //Using Method Syntax
+            //This method belongs to System.Collections.Generic namespace
+            var IsExistsMS1 = namesList.Contains("Shruti");
+            //This method belongs to System.Linq namespace
+            var IsExistsMS2 = namesList.AsEnumerable().Contains("Shruti");
+            //Using Query Syntax
+            var IsExistsQS = (from num in namesList
+                              select num).Contains("Shruti");
+            Console.WriteLine(IsExistsQS);
+            Console.ReadKey();
+        }
+    }
+}
